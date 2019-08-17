391a4f582837d3e1413fed2a32135257958f37dc
Daniel Friyia <dan@trainwithpush.com>
2019-08-17 18:52:19 -0400

    credits page

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from "@angular/common";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "../home/home.component";
 import { InfoComponent } from "../info/info.component";
+import { CreditsPageComponent } from "../credits-page/credits-page.component";
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
+  { path: "credits", component: CreditsPageComponent },
   { path: "info/:id", component: InfoComponent },
   { path: "", redirectTo: "/home", pathMatch: "full" }
 ];
